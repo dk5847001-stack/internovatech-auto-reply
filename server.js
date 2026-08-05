d4cf367da5029b73c51edddb6710b3e7c6307875
Dilkhush Kumar <dk5847001@gmail.com>
2026-08-05 18:32:52 +0530

    server.js update

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
 require("dotenv").config();
 
+const express = require("express");
 
 const connectDB = require("./config/db");
-
 const startMailListener = require("./services/mailListener");
 
+const app = express();
 
+const PORT = process.env.PORT || 5000;
 
-(async()=>{
+// Health Check
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "InternovaTech Auto Reply Bot Running 🚀"
+    });
+});
 
+(async () => {
+    try {
 
-try{
+        console.log("🚀 Starting InternovaTech Auto Reply Bot...");
 
+        await connectDB();
 
-console.log(
-"🚀 Starting InternovaTech Auto Reply Bot..."
-);
+        await startMailListener();
 
+        app.listen(PORT, () => {
+            console.log(`🌐 Server running on port ${PORT}`);
+        });
 
+    } catch (err) {
 
-await connectDB();
+        console.error(err);
 
+        process.exit(1);
 
-
-await startMailListener();
-
-
-
-}
-catch(error){
-
-
-console.error(
-"❌ Server Error:",
-error.message
-);
-
-
-}
-
-
+    }
 })();
